387a4f2a26dfd392cb305337618d11ca3b8eaf4d
mohammadrizvy <rizvyhq1@gmail.com>
2023-04-10 23:48:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-800">Hello world!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum voluptatem architecto, beatae, repellendus in quisquam sequi quasi iste alias sed ea, tempora porro ducimus magni. Magnam recusandae sint officia atque?</h1>
+    </div>
+  );
+};
+
+export default App;
