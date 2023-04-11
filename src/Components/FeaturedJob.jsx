64108d2cf3d2c3785b691df915975abb29f46e50
mohammadrizvy <rizvyhq1@gmail.com>
2023-04-12 02:46:50 +0600

    FeaturedJob JSON created

--- src/Components/FeaturedJob.jsx
+++ src/Components/FeaturedJob.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 const FeaturedJob = () => {
     return (
       <div>
-        <h1 className="text-center mt-20 text-3xl font-extrabold text-black">
-          Featured Jobs
-        </h1>
-        <p className="font-semibold text-slate-500 text-center mt-4 text-sm">
-          Explore thousands of job opportunities with all the information you
-          need. Its your future
-        </p>
+        <div className="">
+          <h1 className="text-center mt-20 text-3xl font-extrabold text-black">
+            Featured Jobs
+          </h1>
+          <p className="font-semibold text-slate-500 text-center mt-4 text-sm">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future
+          </p>
+        </div>
+        <div className="">
+          
+        </div>
       </div>
     );
 };
