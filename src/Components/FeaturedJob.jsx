e335b1f6ac26e9c263add872bc8dd62c104037fc
mohammadrizvy <rizvyhq1@gmail.com>
2023-04-12 22:36:56 +0600

    See all jobs button added

--- src/Components/FeaturedJob.jsx
+++ src/Components/FeaturedJob.jsx
@@ -1,45 +1,46 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import FeaturedSingleJob from './FeaturedSingleJob';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import FeaturedSingleJob from "./FeaturedSingleJob";
 
 const FeaturedJob = () => {
-
-  const[ seeAll,setSeeAll ] = useState(false);
+  const [seeAll, setSeeAll] = useState(false);
   const hadleSeeAll = () => {
-    setSeeAll(true)
-  }
+    setSeeAll(true);
+  };
   const jobs = useLoaderData();
-    // console.log(jobs)
+  // console.log(jobs)
 
-    return (
-      <div>
-        <div className="">
-          <h1 className="text-center mt-20 text-3xl font-extrabold text-black">
-            Featured Jobs
-          </h1>
-          <p className="font-semibold text-slate-500 text-center mt-4 text-sm">
-            Explore thousands of job opportunities with all the information you
-            need. Its your future
-          </p>
-        </div>
-        <div className="the-container grid gap-4 mb-8 lg:grid-cols-2 sm:grid-cols-1">
-          {/* {
+  return (
+    <div>
+      <div className="">
+        <h1 className="text-center mt-20 text-3xl font-extrabold text-black">
+          Featured Jobs
+        </h1>
+        <p className="font-semibold text-slate-500 text-center mt-4 text-sm">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div className="the-container grid gap-4 mb-8 lg:grid-cols-2 sm:grid-cols-1">
+        {/* {
           jobs.map( job => <FeaturedSingleJob
           key={job.id}
           job ={job}
           ></FeaturedSingleJob>)
          } */}
 
-          {jobs.slice(0, seeAll ? 6 : 4).map((job) => (
-            <FeaturedSingleJob key={job.id} job={job}></FeaturedSingleJob>
-          ))}
-        </div>
+        {jobs.slice(0, seeAll ? 6 : 4).map((job) => (
+          <FeaturedSingleJob key={job.id} job={job}></FeaturedSingleJob>
+        ))}
+      </div>
 
-        <div className='text-center'>
-          <div className="btn the-button" onClick={hadleSeeAll}>See all jobs</div>
+      <div className="text-center">
+        <div className="btn the-button" onClick={hadleSeeAll}>
+          See all jobs
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default FeaturedJob;
