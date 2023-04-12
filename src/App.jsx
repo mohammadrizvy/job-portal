15399ca047a88cd91e98999c7eea5705e4ae258f
mohammadrizvy <rizvyhq1@gmail.com>
2023-04-12 19:02:20 +0600

    Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div>
      <Header></Header>
      <Outlet></Outlet>
-     {/* Footer here */}
+     <Footer></Footer>
     </div>
   );
 };
